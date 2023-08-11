5fc59074954846c85d2eeef918c749bf465d81b7
MartinEskaros <martineskaros@gmail.com>
2023-08-11 00:46:16 -0400

    playRound function done

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ function getComputerChoice(){
     
     return options[choice];
 }
+function roundPlayed(playerSelection,computerSelection){
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
+        return "Tie! You Both Picked " +playerSelection;
+    }
+    else if(playerSelection.toLowerCase()==="rock"){
+        if(computerSelection.toLowerCase()==="paper"){
+            return "You Lose! Paper beats Rock"
+        }else{
+            return "You Won! Rock beats Scissors"
+        }
+    }else if(playerSelection.toLowerCase()==="paper"){
+        if(computerSelection.toLowerCase()==="rock"){
+            return "You Won! Paper beats Rock"
+        }else{
+            return "You Lose! Scissors beats Paper"
+        }
+    }else if(playerSelection.toLowerCase()==="scissors"){
+        if(computerSelection.toLowerCase()==="rock"){
+            return "You Lose! Rock beats Scissors"
+        }else{
+            return "You Won! Scissors beats Paper"
+        }
+    } 
+}
