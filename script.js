5d061c313522b856fb3db430fe2c7dcf6f67f1f0
MartinEskaros <martineskaros@gmail.com>
2023-08-12 02:01:42 -0400

    game function done

--- script.js
+++ script.js
@@ -4,27 +4,70 @@ function getComputerChoice(){
     
     return options[choice];
 }
-function roundPlayed(playerSelection,computerSelection){
+function playRound(playerSelection,computerSelection){
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()){
-        return "Tie! You Both Picked " +playerSelection;
+        return 0;
     }
     else if(playerSelection.toLowerCase()==="rock"){
         if(computerSelection.toLowerCase()==="paper"){
-            return "You Lose! Paper beats Rock"
+            return 1;
         }else{
-            return "You Won! Rock beats Scissors"
+            return 2;
         }
     }else if(playerSelection.toLowerCase()==="paper"){
         if(computerSelection.toLowerCase()==="rock"){
-            return "You Won! Paper beats Rock"
+            return 1;
         }else{
-            return "You Lose! Scissors beats Paper"
+            return 2;
         }
     }else if(playerSelection.toLowerCase()==="scissors"){
         if(computerSelection.toLowerCase()==="rock"){
-            return "You Lose! Rock beats Scissors"
+            return 1;
         }else{
-            return "You Won! Scissors beats Paper"
+            return 2;
         }
     } 
 }
+function game(){
+    let wins=0;
+    let losses=0;
+    let ties=0.
+    for (let i=0; i<5;i++){
+        var selection;
+        do{
+            selection = prompt("Enter choice");
+            if(selection !=="rock" && selection !=="paper" && selection !=="scissors"){
+                alert("Input is incorrect");
+            }
+        }while (selection !=="rock" && selection !=="paper" && selection !=="scissors")
+
+
+        let result = playRound(selection,getComputerChoice())
+        
+        if (result === 1){
+            wins++;
+            alert("win");
+            console.log("Win");
+            
+        }
+        else if (result === 2){
+            losses++   
+            alert("Loss");
+            console.log("Loss");
+        }else{
+            ties++;
+            alert("Tie");
+            console.log("Tie")
+        }
+        
+    }
+    if(wins>losses){
+        return "You Won! The score is "+wins+" - "+losses;
+    } else if(wins<losses){
+        return "You Lost! The score is "+wins+" - "+losses;
+    }else{
+        return "It's a tie!"
+    }
+}
+alert(game());
+console.log(game());
